781a3d00e71863aa7d548978cf5f4d242d1f9978
Ogunshina Ismail <ismailogunshina@gmail.com>
2022-10-16 06:52:07 +0100

    complete folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Image1 } from "./assets";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Development</h1>
+      <h1 className="text-3xl font-bold underline bg-blue-ribbon-500">Development</h1>
+      <img src={Image1} alt="" />
     </div>
   );
 }
---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+// IMAGES
+export { default as Image1 } from './images/Boma.png';
+export { default as authBg } from './images/auth-bg.svg';
+
+// ICONS
