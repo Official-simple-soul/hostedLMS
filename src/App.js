0bd37efce2b4f16ee1104b3f620a2a74a40cd2cb
Ogunshina Ismail <ismailogunshina@gmail.com>
2022-10-16 19:23:24 +0100

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Image1 } from "./assets";
+import Routes from "./routes/Routes";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline bg-blue-ribbon-500">Development</h1>
       <img src={Image1} alt="" />
+      <Routes />
     </div>
   );
 }
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../pages/authentication/Login';
+import SIgnUp from '../pages/authentication/SIgnUp';
 
-const Routes = () => {
+const AllRoutes = () => {
   return (
-    <div>Routes</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/sign-up' element={<SIgnUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default Routes
+export default AllRoutes
