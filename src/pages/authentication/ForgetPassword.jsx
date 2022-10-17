cff92794126a181c8ab6cc8c84f0652e035c2a4e
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2022-10-17 19:58:22 +0100

    Made a few editing on the fonts

--- src/pages/authentication/ForgetPassword.jsx
+++ src/pages/authentication/ForgetPassword.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './ForgetPassword.css'
 import { AiFillEye } from 'react-icons/ai';
 import Modal from '../../components/authentication/Modal'
-import { Good, three } from "../../assets/index";
+import { Good, three, Logo } from "../../assets/index";
 import { ToastContainer, toast } from 'react-toastify';
 
 const ForgetPassword = () => {
@@ -56,7 +56,7 @@ const ForgetPassword = () => {
         <div className='bg-form '>
             <div className='inner '>
                 <div className='text flex flex-col items-center justify-center'>
-                {/* <img src={Logo} height = {50} width= {50} alt='logo' className='pt-3' /> */}
+                <img src={Logo} height = {50} width= {50} alt='logo' className='pt-3' />
                     <h2 className='pt-5'>Create new password</h2>
                     <div className='flex gap-5 flex-wrap pt-3  '>
                         <img src={Good} alt='good' />
@@ -66,7 +66,7 @@ const ForgetPassword = () => {
                     <p className='pt-3'>Kindly set a new password and confirm the password</p>
                 </div>
                 <form onSubmit={handleSubmit}>
-                    <div className='flex flex-col mt-3 relative'>
+                    <div className='flex flex-col mt-5 relative'>
                         <label>Password</label>
                         <input placeholder='Type new password'style={{'border': '1px solid black'}}
                         className='px-5 mt-2'
@@ -74,7 +74,7 @@ const ForgetPassword = () => {
                         />
                         <AiFillEye className='absolute right-2 bottom-3' />
                     </div> 
-                    <div className='flex flex-col mt-4 relative'>
+                    <div className='flex flex-col mt-5 relative'>
                         <label>Confirm Password</label>
                         <input placeholder='Type your password again' 
                         style={{'border': '0.1px solid black'}}
@@ -83,8 +83,8 @@ const ForgetPassword = () => {
                        
                         />
                     </div> 
-                    <button className='mt-4 bg-blue-500'
-                        style={{'border': '1px solid black', 'width' : '100%', 'color':'white'}}>
+                    <button className='mt-5 bg-blue-500'
+                        style={{'width' : '100%', 'color':'white'}}>
                          Save Password</button>
                         <div className='text-center pt-3'>
                             <span>Remember Password?</span>
