7612d66e1706347433c3f91f73aeaad692acc12d
chuma6619 <chuma6619@gmail.com>
2022-10-19 00:27:37 +0100

    login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Login from "./pages/login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Development</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
