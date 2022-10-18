40363223810381c70397ff4360ba280964bba9fc
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2022-10-18 21:19:43 +0100

    progress bar and multi page func implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Image1 } from "./assets";
-import SignUp from "./pages/authentication/Signup1";
+import Form from "./pages/authentication/signup/Form";
 
 function App() {
   return (
     <div className="App">
-      { <h1 className="text-3xl font-bold underline bg-blue-ribbon-500">Development</h1>
-      <img src={Image1} alt="" /> }
+      <h1 className="text-3xl font-bold underline bg-blue-ribbon-500">Development</h1>
+      <img src={Image1} alt="" /> 
     </div>
   );
 }
---
+++ src/pages/authentication/signup/components/SignUpTwo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUpTwo = () => {
+  return (
+    <div>SignUp2</div>
+  )
+}
+
+export default SignUpTwo
