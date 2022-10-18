2ff42f6cbed537abcf2869dd36e50afcd989bea9
muak1 <muak1ayomide@gmail.com>
2022-10-18 05:35:29 +0100

    cleared up some unused lines

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import VerifyEmail from "./pages/authentication/VerifyEmail";
 import "./App.css"
 
 
 function App() {
   return (
     <div className="App">
-      <VerifyEmail />
     </div>
   );
 }
