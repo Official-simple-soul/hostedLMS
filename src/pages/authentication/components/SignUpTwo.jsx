8941d48e32a2a69c1b9a13b610fdf391edaf6219
innthomas <innthomas00158@gmail.com>
2022-10-20 09:38:40 +0100

    edited css file

--- src/pages/authentication/components/SignUpTwo.jsx
+++ src/pages/authentication/components/SignUpTwo.jsx
@@ -15,11 +15,11 @@ function SignUpTwo() {
     <div className="App w-[100vw] h-[200vh] flex items-center justify-center "style={{backgroundImage:`url(${Background})`}}>
       <div className='container flex w-[80vw] max-h-[180vh] rounded-2xl' >
         <div className='signup__leftcard w-[50vw] h-[170vh] text-white flex flex-col items-center mt-[10rem]'>
-          <h1 className='signup__leftcard-h1'>Kodecamp LMS</h1>
+          <h1 className='signup__leftcard-h1 text-[30px] '>Kodecamp LMS</h1>
           <p className='signup__leftcard-p'>Learning has been made simple, interesting and fun.</p>
         </div>
-        <div className='signup__rightcard'>
-          <div className='signup__form-container'>
+        <div className='signup__rightcard w-[50vw] h-[170vh] mt-[2rem]'>
+          <div className='signup__form-container flex flex-col items-center'>
             <img className='signup__img' src={KodeImg} alt="kodeImg" />
             <h1 className='signup__form-h1'>Create account</h1>
             <div className='signup__form-icons m-3 flex'>
@@ -32,11 +32,11 @@ function SignUpTwo() {
             </div>
             <form action="" className='signup__form'>
               <div className='signup__form-div'>
-              <label className='signup__form-label' htmlFor="">Age</label>
-              <input className='signup__form-input' type="text" placeholder='Type your age'/>
+              <label className='signup__form-label block' htmlFor="">Age</label>
+              <input className='signup__form-input border-2' type="text" placeholder='Type your age'/>
               </div>
               <div className='signup__form-div'>
-              <label className='signup__form-label' htmlFor="">Gender</label>
+              <label className='signup__form-label block' htmlFor="">Gender</label>
               <select className='signup__form-input select' name="gender" id="gender-select">
                   <option value="">Select your gender</option>
                   <option value="male">Male</option>
@@ -44,7 +44,7 @@ function SignUpTwo() {
               </select>
               </div>
               <div className='signup__form-div'>
-              <label className='signup__form-label' htmlFor="">Track</label>
+              <label className='signup__form-label block' htmlFor="">Track</label>
               <select className='signup__form-input select' name="track" id="track-select">
                   <option value="">Select track</option>
                   <option value="frontend">Frontend</option>
@@ -53,7 +53,7 @@ function SignUpTwo() {
               </select>
               </div>
               <div className='signup__form-div'>
-              <label className='signup__form-label' htmlFor="">Experience level</label>
+              <label className='signup__form-label block' htmlFor="">Experience level</label>
               <select className='signup__form-input select' name="experience" id="experience-select">
                   <option value="">Select your level of experience</option>
                   <option value="newbie">Newbie</option>
@@ -65,7 +65,7 @@ function SignUpTwo() {
               </select>
               </div>
               <div className='signup__form-div'>
-              <label className='signup__form-label' htmlFor="">Employment status</label>
+              <label className='signup__form-label block' htmlFor="">Employment status</label>
               <select className='signup__form-input select' name="education" id="employment-select">
                 <option value="">Select employment status</option>
                 <option value="student">Student</option>
@@ -76,7 +76,7 @@ function SignUpTwo() {
             </select>
               </div>
               <div className='signup__form-div'>
-              <label className='signup__form-label' htmlFor="">Educational qualification</label>
+              <label className='signup__form-label block' htmlFor="">Educational qualification</label>
               <select className='signup__form-input select' name="education" id="education-select">
                   <option value="">Select your highest level of education</option>
                   <option value="primary">Primary</option>
@@ -88,7 +88,7 @@ function SignUpTwo() {
               </select>
               </div>
               <div className='signup__form-div'>
-              <label className='signup__form-label' htmlFor="">How did you hear about us?</label>
+              <label className='signup__form-label block' htmlFor="">How did you hear about us?</label>
               <select className='signup__form-input select' name="how" id="how-select">
                   <option value="">Select one</option>
                   <option value="twitter">Twitter</option>
