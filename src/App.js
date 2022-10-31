d98d1b65f1107fcc5be01adc866f8b8faf030b28
Ogunshina Ismail <ismailogunshina@gmail.com>
2022-10-31 22:40:33 +0100

    forgot password merge

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Routes from "./routes/Routes";
 function App() {
   return (
     <div className="App">
-      
       <Routes />
     </div>
   );
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Bg from '../pages/authentication/Bg';
+import Forgotpsw from '../pages/authentication/Forgotpsw';
 import Login from '../pages/authentication/Login';
 import NewPassword from '../pages/authentication/NewPassword';
-
 import SignUp from '../pages/authentication/SIgnUp'
-
 import VerifyEmail from '../pages/authentication/VerifyEmail';
 
 
@@ -13,9 +13,10 @@ const AllRoutes = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<SignUp />} />
-       
         <Route path='/login' element={<Login />} />
         <Route path='/verify-email' element={<VerifyEmail />} />
+        <Route path='forgot-password' element={<Forgotpsw />} />
+        <Route path='bg' element={<Bg />} />
         <Route path='forgot-password/new-password' element={<NewPassword />} />
       </Routes>
     </BrowserRouter>
