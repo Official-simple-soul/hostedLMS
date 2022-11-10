d7315d249d102d0cd99d41fb357cf73fa4d378ab
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2022-11-10 18:34:50 +0100

    test: pull to test local work

--- src/pages/admin/components/AdminDashboard.jsx
+++ src/pages/admin/components/AdminDashboard.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import AdminNav from './AdminNav'
+import CurriculumAdmin from './CurriculumAdmin'
+import Sidebar from './Sidebar'
 
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
+    <div className='bg-[#F5F5F5] flex '>
+      <Sidebar/>
+     <div className='flex items-center ml-[24px] mr-[24px] mt-[12px] flex-col w-[100%]'>
+       <AdminNav/>
+       <CurriculumAdmin/>
+     </div>
+    </div>
   )
 }
 
