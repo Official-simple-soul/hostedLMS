6ed026c1a5dc10cb8472c2de30ac546dd190f8c1
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-12-22 17:31:47 +0100

    worked on

--- src/pages/student/components/ProfileEdit.jsx
+++ src/pages/student/components/ProfileEdit.jsx
@@ -3,21 +3,27 @@ import React from "react";
 
 export const ProfileEdit = () => {
     return (
-        <form action="">
-            <label htmlFor="">Full Name</label>
-            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
-            <label htmlFor="" className="mt-3">Full Name</label>
-            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
-            <label htmlFor="" className="mt-3">Full Name</label>
-            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
-            <label htmlFor="" className="mt-3">Full Name</label>
-            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
-            <label htmlFor="" className="mt-3">Full Name</label>
-            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
-            <label htmlFor="" className="mt-3">Full Name</label>
-            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
-
+        <>
+        <form action="" className="mt-4">
+            <label htmlFor="" className="block text-[grey]">Full Name</label>
+            <input type="text" name="text" className="md:py-1 px-1 border border-[grey] w-full md:w-96"/>
+            <label htmlFor="" className="mt-3 block text-[grey]">Username</label>
+            <input type="text" name="text" className="md:py-1 px-1 border border-[grey] w-full md:w-96"/>
+            <label htmlFor="" className="mt-3 block text-[grey]">Email Address</label>
+            <input type="text" name="text" className="md:py-1 px-1 border border-[grey] w-full md:w-96"/>
+            <label htmlFor="" className="mt-3 block text-[grey]">Date of Birth</label>
+            <input type="text" name="text" className="md:py-1 px-1 border border-[grey] w-full md:w-96"/>
+            <label htmlFor="" className="mt-3 block text-[grey]">Gender</label>
+            <input type="text" name="text" className="md:py-1 px-1 border border-[grey] w-full md:w-96"/>
+            <label htmlFor="" className="mt-3 block text-[grey]">Phone Number</label>
+            <input type="text" name="text" className="md:py-1 px-1 border border-[grey] w-full md:w-96"/>
+            
         </form>
+        <div className="flex mt-4 space-x-4">
+            <button className="px-4 border border-[grey] text-sm py-1 rounded-md">Cancel</button>
+            <button className="px-4 bg-blue-ribbon-500 text-white text-sm py-1 rounded-md">Save Changes</button>
+        </div>
+        </>
     )
 }
 
