fa4d8897865532dc598862030cb02a13227f8922
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-12-22 14:02:42 +0100

    updated development

---
+++ src/pages/student/components/ProfileEdit.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+export const ProfileEdit = () => {
+    return (
+        <form action="">
+            <label htmlFor="">Full Name</label>
+            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
+            <label htmlFor="" className="mt-3">Full Name</label>
+            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
+            <label htmlFor="" className="mt-3">Full Name</label>
+            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
+            <label htmlFor="" className="mt-3">Full Name</label>
+            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
+            <label htmlFor="" className="mt-3">Full Name</label>
+            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
+            <label htmlFor="" className="mt-3">Full Name</label>
+            <input type="text" name="text" className="py-1 border-[grey] w-96"/>
+
+        </form>
+    )
+}
+
+
+export const ProfileEdittwo = () => {
+
+}
+
--- src/pages/student/components/StudentProfile.jsx
+++ src/pages/student/components/StudentProfile.jsx
@@ -4,6 +4,8 @@ import ProfilePic from '../../../assets/images/Male 01.png'
 
 const StudentProfile = () => {
   const [uploadOption, setUploadOption] = useState(false)
+  const [profilePic, setProfilePic] = useState(ProfilePic)
+  const [profileOption, setProfileOption] = useState('Personal details')
   const newD = JSON.parse(localStorage.getItem('userinfo'))
 
     const [value, setValue] = useState({
@@ -23,13 +25,25 @@ const StudentProfile = () => {
     </div>
     <div className="bg-white rounded-lg px-8 py-4 mt-6">
       <div className="flex space-x-12">
-        <h1>Personal details</h1>
-        <h1>Professional details</h1>
+        <h1 className={`${profileOption==='Personal details'?"border-b border-black":"text-blue-ribbon-500"} cursor-pointer`} onClick={(e)=>setProfileOption(e.currentTarget.innerText)}>Personal details</h1>
+        <h1 className={`${profileOption==='Professional details'?"border-b border-black":"text-blue-ribbon-500"} cursor-pointer`} onClick={(e)=>setProfileOption(e.currentTarget.innerText)}>Professional details</h1>
       </div>
+      {
+        profileOption === 'Personal details'?
+        <>
       <div className="md:flex items-center md:space-x-12 mt-8">
-        <div className="img relative" onClick={()=>setUploadOption(!uploadOption)}>
-          <img src={ProfilePic} alt="" />
+        <div className="img relative cursor-pointer" onClick={()=>setUploadOption(!uploadOption)}>
+          <div className="border w-48 h-48 rounded-full bg-white">
+            <img src={profilePic} alt="" />
+          </div>
           <i class="fa-solid fa-camera absolute right-16 bottom-6 md:right-4 text-blue-ribbon-500 md:bottom-4 bg-white p-3 rounded-full"></i>
+          <div className={`${uploadOption?'block':'hidden'} upload absolute top-0 -right-10 shadow-lg bg-white p-4 rounded-lg`}>
+            <div className="file cursor-pointer">
+              <label htmlFor="img"><i class="fa-solid fa-image mr-1"></i>Upload Picture</label>
+              <input type="file" id='img' className='hidden'/>
+            </div>
+            <h1 className='text-[red] mt-4 cursor-pointer' onClick={()=>setProfilePic(null)}>Delete Picture</h1>
+          </div>
         </div>
         <div className="right space-y-4">
           <h1 className='text-lg md:text-2xl'>{fullname}</h1>
@@ -58,6 +72,26 @@ const StudentProfile = () => {
             </ul>
           </div>
       </div>
+        </>
+      :
+        <>
+          <div className="grid grid-cols-2 md:grid-cols-4 mt-8">
+            <div className="left">
+              <ul className='space-y-4'>
+                <li className='border-b pb-1'>Employement Status</li>
+                <li className='border-b pb-1'>Education Qualification</li>
+              </ul>
+            </div>
+            <div className="right md:col-span-3">
+              <ul className='space-y-4'>
+                <li className='border-b pb-1 pl-5 font-bold'>Unemployed</li>
+                <li className='border-b pb-1 pl-5 font-bold'>PhD</li>
+              </ul>
+            </div>
+          </div>
+        </>
+      }
+      
       <button className='text-sm text-white bg-blue-ribbon-100 px-4 py-2 rounded-lg mt-4' type='button' disabled><i class="fa-solid fa-certificate mr-2"></i>View Certificate</button>
     </div>
     </>
