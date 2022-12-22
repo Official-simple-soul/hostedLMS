61235d7f528b3f949b2ebf63b17a1307bc302721
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2022-12-22 10:33:42 +0100

    FEAT: student curriculum added

--- src/pages/student/components/StudentCurriculum.jsx
+++ src/pages/student/components/StudentCurriculum.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DownArrow from '../../../assets/icons/down-arrow.svg'
+import Cross from '../../../assets/icons/cross.svg'
+import FilterLevel from '../../../components/curriculum/curriculum-components/FilterLevel'
+import CurriculumPageWrapper from '../../../components/curriculum/CurriculumPageWrapper'
 
 const StudentCurriculum = () => {
+ const [studentCurr,setStudentCurri]= useState(true)
+  const CreateNew =()=>{
+    setStudentCurri(studentCurr)
+  }
 
    return (
     <>
       <div>
-         cy=ujhsa
+         <CurriculumPageWrapper studentCurr={studentCurr} setStudentCurri={setStudentCurri} CreateNew={CreateNew}>
+          <h1>Curriculum</h1>
+         </CurriculumPageWrapper>
       </div>
     </>
   )
