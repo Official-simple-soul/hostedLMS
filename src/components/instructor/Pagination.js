4abbceb1aee116671e534d8378ee65e8d9a60586
ismail <ismailogunshina@gmail.com>
2023-01-01 08:28:20 +0800

    chore: Trainer Task Submission and Pagination implementation

---
+++ src/components/instructor/Pagination.js
@@ -0,0 +1,44 @@
+import React from 'react'
+// import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
+
+const Pagination = ({ totalPosts, postsPerPage, setCurrentPage, currentPage }) => {
+    const pages = [];
+
+    for(let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pages.push(i)
+    }
+
+  return ( 
+    <div className='flex items-center justify-end mt-14'>
+        <div className='flex gap-8 items-center'>
+            <p 
+                className={currentPage <= 1 ? "hidden" : "block text-xs text-[#585858]"}
+                onClick={() => setCurrentPage(currentPage - 1) }
+            >
+                Prev
+            </p>
+                
+                    {console.log(pages)}
+                    {pages && pages.map((page, index) =>{
+                        return <p 
+                                    key={index} 
+                                    onClick={() => setCurrentPage(page)}
+                                    className={currentPage === page ? "text-xs bg-[#0D6EFD] rounded py-4 px-5 text-center text-white" : "text-xs"}
+                                >
+                                    {page}
+                                </p>
+                    })}
+               
+            <p 
+                className={currentPage === pages[pages.length - 1] ? "hidden" : "block text-xs text-[#585858]"}
+                onClick={() => setCurrentPage(currentPage + 1) }
+            >
+                Next
+            </p>
+
+        </div>
+    </div>
+  )
+}
+
+export default Pagination
--- src/components/instructor/TaskSubmissionLink.jsx
+++ src/components/instructor/TaskSubmissionLink.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
-import { Avatar } from '../../assets'
 import { BsThreeDotsVertical } from 'react-icons/bs'
+import useFetch from '../../hooks/useFetch';
+import Pagination from './Pagination';
 
 const TaskSubmissionLink = () => {
     const [ showModal, setShowModal ] = useState(false)
+    const [ currentPage, setCurrentPage ] = useState(1);
+    const [postsPerPage, setPostsPerPage] = useState(3);
+    const { data: tasks, isPending, error } = useFetch("http://localhost:8000/submissions")
+
+    const lastPostIndex = currentPage * postsPerPage
+    const firstPostIndex = lastPostIndex - postsPerPage
+    const currentPosts = tasks && tasks.slice(firstPostIndex, lastPostIndex)
+
   return (
     <div className='mt-4'>
         <p className="text-xl">Redesign KodeCamp Website</p>
@@ -12,44 +21,65 @@ const TaskSubmissionLink = () => {
         <div className="flex mt-4 items-center gap-[15rem]">
             <div className='flex items-center gap-2 bg-white py-3 px-6'>
                 <input type="checkbox" name="" id="" />
-                <p className="text-grey-500 text-sm">Select all students</p>
+                <p className="text-grey-500 text-sm">Select all students</p> 
             </div>
 
             <p className='text-base font-normal'>Showing 1-10 of 78 submissions</p>
         </div>
 
-        <div className="flex flex-col gap-3 mt-8">
-            <div className="grid grid-cols-7 gap-1 bg-white px-6 py-3 rounded-xl">
-                <div className='border-r px-3 flex items-center gap-8 col-span-2'>
-                    <input type="checkbox" name="" id="" />
-                    <div className="flex gap-4 items-center">
-                        <img src={Avatar} alt="" />
-                        <p className='text-lg font-medium text-grey-500'>Sotonye Ebadan</p>
-                    </div>
-                </div>
+        {isPending && <div className='text-sm'>Loading.....</div>}
+        {error && <div className='text-sm bg-red-500 p-6 '>{ error }</div>}
 
-                <div className='border-r px-3 col-span-3 flex flex-cols gap-2'>
-                    <div className="w-full flex items-center justify-between">
-                        <p className="text-sm text-grey-500">Redesign KodeCamp Website</p>
-                        <p className="text-sm text-grey-500">05:45PM . 12/03/2022</p>
-                    </div>
-                </div>
-
-                <div className='px-3 flex items-center justify-between col-span-2'>
-                    <div className="flex items-center gap-4">
-                        <div className="flex flex-col items-center">
-                            <p className="text-lg">Grade</p>
-                            <p className="text-xs text-red-500">Not Graded</p>
+        {currentPosts && currentPosts.map((data) => (
+                <div className="flex flex-col gap-3 mt-8" key={data.id}>
+                    <div className="grid grid-cols-7 gap-1 bg-white px-6 py-3 rounded-xl">
+                        <div className='border-r px-3 flex items-center gap-8 col-span-2'>
+                            <input type="checkbox" name="" id="" />
+                            <div className="flex gap-4 items-center">
+                                <img src={data.avatar} alt="" />
+                                <p className='text-lg font-medium text-grey-500'>{data.name}</p>
+                            </div>
                         </div>
-                        <input type="numbet" className='h-[56px] w-[86px] rounded-lg border' />
+
+                        <div className='border-r px-3 col-span-3 flex flex-col gap-2'>
+                            <div className="w-full flex items-center justify-between">
+                                <p className="text-sm text-grey-500">Redesign KodeCamp Website</p>
+                                <p className="text-sm text-grey-500">05:45PM . 12/03/2022</p>
+                            </div>
+                            <div className='border border-gray-100 p-2'>
+                                <p>{data.url}</p>
+                            </div>
+                        </div>
+
+                        <div className='px-3 flex items-center justify-between col-span-2'>
+                            <div className="flex items-center gap-4">
+                                <div className="flex flex-col items-center">
+                                    <p className="text-lg">Grade</p>
+                                    <p className="text-xs text-red-500">Not Graded</p>
+                                </div>
+                                <input type="number" className='h-[56px] w-[86px] rounded-lg border px-2' placeholder={data.grade} />
+                            </div>
+                            <BsThreeDotsVertical 
+                                className='text-lg text-[#303030]' 
+                                onClick={() => setShowModal(!showModal)}
+                            />
+                        </div>
                     </div>
-                    <BsThreeDotsVertical 
-                        className='text-lg text-[#303030]' 
-                        onClick={() => setShowModal(!showModal)}
-                    />
                 </div>
-            </div>
-        </div>
+            )
+        )}
+
+        <Pagination 
+            totalPosts={tasks && tasks.length} 
+            postsPerPage={postsPerPage}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            setPostsPerPage={setPostsPerPage}
+        />
+
+
+
+
 
         {showModal ? <div>Heloo</div> : null}
     </div>
